8d4739a19e3411485789c75c764e6f3ab547aebc
tevinstein <tevinsa@yahoo.com>
2016-12-08 19:42:31 +0700

    test api

--- test/crud-item.test.js
+++ test/crud-item.test.js
@@ -3,12 +3,29 @@ const mocha = require('mocha')
 const chai = require('chai')
 const expect = chai.expect
 const models = require('../models')
+const chaiHttp = require('chai-http')
 const Item = models.Item
 const Category = models.Category
 
+chai.use(chaiHttp)
+
+var new_item = {
+    name: 'Hacktiv 8 Shirt',
+    description: 'Been using this for 2012, color is not bad at all.',
+    dimension: 'M',
+    material: 'silk',
+    photo: 'http://i.imgur.com/N5QkKfH.jpg',
+    color: 'Orange',
+    status: 'Up for barter'
+}
+
 describe("Items", function() {
     beforeEach(function(done) {
-        Item.destroy().then((data) => {
+        Item.destroy({
+            where: {
+
+            }
+        }).then((data) => {
             done()
         })
     })
@@ -41,31 +58,23 @@ describe("Items", function() {
 
     describe("Get An Item", () => {
         it('it should GET an item', (done) => {
-            Item.create({
-                name: 'Hacktiv 8 Shirt',
-                description: 'Been using this for 2012, color is not bad at all.',
-                dimension: 'M',
-                material: 'silk',
-                photo: 'http://i.imgur.com/N5QkKfH.jpg',
-                color: 'Orange',
-                status: 'Up for barter'
-            }).then((item) => {
+            Item.create(new_item).then((item) => {
                 Item.findOne({
-                	where: {
-                		id: data.id
-                	}
+                    where: {
+                        id: item.id
+                    }
                 }).then((data) => {
-                	expect(data[0]).to.haveOwnProperty('id')
-                	expect(data[0]).to.haveOwnProperty('name')
-                	expect(data[0]).to.haveOwnProperty('description')
-                	expect(data[0]).to.haveOwnProperty('dimension')
-                	expect(data[0]).to.haveOwnProperty('material')
-                	expect(data[0]).to.haveOwnProperty('photo')
-                	expect(data[0]).to.haveOwnProperty('color')
-                	expect(data[0]).to.haveOwnProperty('status')
-                	expect(data[0].name).to.equal('Hacktiv 8 Shirt')
-                	expect(data[0].color).to.equal('Orange')
-                	done()
+                    expect(data).to.haveOwnProperty('id')
+                    expect(data).to.haveOwnProperty('name')
+                    expect(data).to.haveOwnProperty('description')
+                    expect(data).to.haveOwnProperty('dimension')
+                    expect(data).to.haveOwnProperty('material')
+                    expect(data).to.haveOwnProperty('photo')
+                    expect(data).to.haveOwnProperty('color')
+                    expect(data).to.haveOwnProperty('status')
+                    expect(data.name).to.equal('Hacktiv 8 Shirt')
+                    expect(data.color).to.equal('Orange')
+                    done()
                 })
             })
         })
@@ -73,15 +82,7 @@ describe("Items", function() {
 
     describe("Post An Item", () => {
         it('it should POST an item', (done) => {
-            Item.create({
-                name: 'Hacktiv 8 Shirt',
-                description: 'Been using this for 2012, color is not bad at all.',
-                dimension: 'M',
-                material: 'silk',
-                photo: 'http://i.imgur.com/N5QkKfH.jpg',
-                color: 'Orange',
-                status: 'Up for barter'
-            }).then((data) => {
+            Item.create(new_item).then((data) => {
                 expect(data).to.haveOwnProperty('id')
                 expect(data).to.haveOwnProperty('name')
                 expect(data).to.haveOwnProperty('description')
@@ -99,15 +100,7 @@ describe("Items", function() {
 
     describe("Update An Item", () => {
         it('it should UPDATE an item', (done) => {
-            Item.create({
-                name: 'Hacktiv 8 Shirt',
-                description: 'Been using this for 2012, color is not bad at all.',
-                dimension: 'M',
-                material: 'silk',
-                photo: 'http://i.imgur.com/N5QkKfH.jpg',
-                color: 'Orange',
-                status: 'Up for bartered'
-            }).then((data) => {
+            Item.create(new_item).then((data) => {
                 Item.update({
                     name: 'Bee Gees Shirt',
                     description: 'Only used for 2 months.',
@@ -141,22 +134,177 @@ describe("Items", function() {
 
     describe("Delete an Item", () => {
         it('it should DELETE an item', (done) => {
-            Item.create({
-                name: 'Hacktiv 8 Shirt',
-                description: 'Been using this for 2012, color is not bad at all.',
-                dimension: 'M',
-                material: 'silk',
-                photo: 'http://i.imgur.com/N5QkKfH.jpg',
-                color: 'Orange',
-                status: 'Up for bartered'
-            }).then((data) => {
+            Item.create(new_item).then((item) => {
                 Item.destroy({
                     where: {
-                        id: data.id
+                        id: item.id
                     }
                 }).then((data) => {
                     expect(data).to.equal(1)
                     done()
+                })
+            })
+        })
+    })
+})
+
+// =======================================
+// API TEST CASES
+// =======================================
+
+describe("Items API", function() {
+    beforeEach(function(done) {
+        Item.destroy({
+            where: {
+
+            }
+        }).then((data) => {
+            done()
+        })
+    })
+
+    describe("Get All Items", () => {
+        it('it should GET all items', (done) => {
+            Item.findAll().then((data) => {
+                chai.request(app)
+                    .get('/api/items')
+                    .end((err, res) => {
+                        expect(res).to.have.status(200)
+                        expect(res.body).that.is.an('array')
+                        expect(res.body).length.should.be.eql(0)
+                        done()
+                    })
+            })
+        })
+    })
+
+    describe("Get All Items By Category", () => {
+        it('it should GET all items', (done) => {
+            Category.findAll().then((categories) => {
+                Item.findAll({
+                    where: {
+                        CategoryId: categories[0].id
+                    }
+                }).then((data) => {
+                    chai.request(app)
+                        .get(`/api/categories/${categories[0].id}`)
+                        .end((err, res) => {
+                            expect(res).to.have.status(200)
+                            expect(res.body).that.is.an('array')
+                            expect(res.body).length.should.be.eql(0)
+                            done()
+                        })
+                })
+            })
+        })
+    })
+
+    describe("Get An Item", () => {
+        it('it should GET an item', (done) => {
+            Item.create(new_item).then((item) => {
+                Item.findOne({
+                    where: {
+                        id: item.id
+                    }
+                }).then((data) => {
+                    chai.request(app)
+                        .get(`/api/items/${data.id}`)
+                        .end((err, res) => {
+                            expect(res).to.have.status(200)
+                            expect(res.body).to.haveOwnProperty('id')
+                            expect(res.body).to.haveOwnProperty('name')
+                            expect(res.body).to.haveOwnProperty('description')
+                            expect(res.body).to.haveOwnProperty('dimension')
+                            expect(res.body).to.haveOwnProperty('material')
+                            expect(res.body).to.haveOwnProperty('photo')
+                            expect(res.body).to.haveOwnProperty('color')
+                            expect(res.body).to.haveOwnProperty('status')
+                            expect(res.body.name).to.equal('Hacktiv 8 Shirt')
+                            expect(res.body.color).to.equal('Orange')
+                            done()
+                        })
+                })
+            })
+        })
+    })
+
+    describe("Post An Item", () => {
+        it('it should POST an item', (done) => {
+            Item.create(new_item).then((data) => {
+                chai.request(app)
+                    .post(`/api/items/${data.id}`)
+                    .end((err, res) => {
+                        expect(res).to.have.status(200)
+                        expect(res.body).to.haveOwnProperty('id')
+                        expect(res.body).to.haveOwnProperty('name')
+                        expect(res.body).to.haveOwnProperty('description')
+                        expect(res.body).to.haveOwnProperty('dimension')
+                        expect(res.body).to.haveOwnProperty('material')
+                        expect(res.body).to.haveOwnProperty('photo')
+                        expect(res.body).to.haveOwnProperty('color')
+                        expect(res.body).to.haveOwnProperty('status')
+                        expect(res.body.name).to.equal('Hacktiv 8 Shirt')
+                        expect(res.body.color).to.equal('Orange')
+                        done()
+                    })
+            })
+        })
+    })
+
+    describe("Update An Item", () => {
+        it('it should UPDATE an item', (done) => {
+            Item.create(new_item).then((data) => {
+                Item.update({
+                    name: 'Bee Gees Shirt',
+                    description: 'Only used for 2 months.',
+                    dimension: 'L',
+                    material: 'cotton',
+                    photo: 'http://i.imgur.com/tg8ylqw.jpg',
+                    color: 'Black',
+                    status: 'Up for barter'
+                }, {
+                    where: {
+                        id: data.id
+                    }
+                }).then(() => {
+                    Item.findOne({
+                        where: {
+                            id: data.id
+                        }
+                    }).then((data) => {
+                        chai.request(app)
+                            .put(`/api/items/${data.id}`)
+                            .end((err, res) => {
+                                expect(res).to.have.status(200)
+                                expect(res.body.name).to.equal('Bee Gees Shirt')
+                                expect(res.body.material).to.equal('cotton')
+                                expect(res.body.color).to.equal('Black')
+                                expect(res.body.status).to.equal('Up for barter')
+                                expect(res.body.description).to.equal('Only used for 2 months.')
+                                expect(res.body.dimension).to.equal('L')
+                                done()
+                            })
+                    })
+                })
+            })
+        })
+    })
+
+    describe("Delete an Item", () => {
+        it('it should DELETE an item', (done) => {
+            Item.create(new_item).then((item) => {
+                Item.destroy({
+                    where: {
+                        id: item.id
+                    }
+                }).then((data) => {
+                    chai.request(app)
+                        .delete(`/api/items/${item.id}`)
+                        .end((err, res) => {
+                            expect(res).to.have.status(200)
+                            expect(res.body).to.equal(1)
+                            done()
+                        })
                 })
             })
         })
