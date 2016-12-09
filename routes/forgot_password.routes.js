05e850a3bc4d31afe32846f724aef6d438d9e216
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-12-09 15:24:54 +0700

    test forgot password done

---
+++ routes/forgot_password.routes.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+var express = require('express');
+var router = express.Router();
+
+const forgotPasswordController = require('../controller/forgot_password.controller');
+
+
+router.get('/', forgotPasswordController.sendRequest)
+router.get('/verify_request_password/:password', forgotPasswordController.resetPassword)
+
+
+module.exports = router;
