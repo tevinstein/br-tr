3e1e6971c7e13ec7b28325143bbd19009c4db59f
ahyanarizky <ahyana.rizky@gmail.com>
2016-12-13 13:37:53 +0700

    edit search method

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -17,7 +17,7 @@ module.exports = {
                 username: data.username,
                 email: data.email,
                 avatar: data.avatar
-            }, process.env.JWT_SECRET, {expiresIn: '1h'})
+            }, process.env.JWT_SECRET, {expiresIn: '168h'})
             res.status(200).json(token)
         }).catch((err) => {
             res.status(500).json(err)
@@ -34,7 +34,7 @@ module.exports = {
                 username: data.username,
                 email: data.email,
                 avatar: data.avatar
-            }, process.env.JWT_SECRET, {expiresIn: '1h'})
+            }, process.env.JWT_SECRET, {expiresIn: '168h'})
             res.status(200).json(token)
         }).catch((err) => {
             res.status(500).json(err)
