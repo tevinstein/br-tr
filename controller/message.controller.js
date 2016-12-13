753c6b31159a32398b13bf366089fcfd9e00a898
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-12-13 16:14:58 +0700

    fix data

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -17,7 +17,7 @@ module.exports = {
                 ItemId: req.params.ItemId
             }
         }).then((data) => {
-            res.status(200).json(err)
+            res.status(200).json(data)
         }).catch((err) => {
             res.status(500).json(err)
         })
